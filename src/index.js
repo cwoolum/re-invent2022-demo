463ebdfe8587763cbde5710bd2fac0de2ae6223e
Christopher Woolum <woolumc@amazon.com>
2022-11-30 00:00:48 +0000

    fix default

--- src/index.js
+++ src/index.js
@@ -4,22 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { ThemeProvider } from "@aws-amplify/ui-react";
-import { Amplify } from "aws-amplify";
-
-import awsconfig from "./aws-exports";
-
-import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./ui-components";
-
-Amplify.configure(awsconfig);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={studioTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
